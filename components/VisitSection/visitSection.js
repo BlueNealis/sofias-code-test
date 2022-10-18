ada5270c54b0261dfa022e77b1e16d6b77f1e924
Blue Jessen <canealisblue@gmail.com>
2022-10-18 14:20:53 -0700

    Add content of left side of visit section to html

--- components/VisitSection/visitSection.js
+++ components/VisitSection/visitSection.js
@@ -19,5 +19,20 @@ export const VisitSection = ({ section, parallax }) => {
     mapLocation,
   } = section.fields;
 
-  return <section id={menuTitle} className={styles.visitSection}></section>;
+  console.log('head',hoursHeadline, 'group', hoursGroups)
+
+  return <section id={menuTitle} className={styles.visitSection}>
+    <div className={styles.topLeftInfo}>
+      <div className={styles.hoursSection}>
+        <h1>{hoursHeadline}</h1>
+        <h1>{hoursGroups[0].fields.title}</h1>
+        <h1>{`${hoursGroups[0].fields.hours[0].fields.startDay} - ${hoursGroups[0].fields.hours[0].fields.endDay}`}</h1>
+        <h1>{`${hoursGroups[0].fields.hours[0].fields.startTime} - ${hoursGroups[0].fields.hours[0].fields.endTime}`}</h1>
+      </div>
+      <div className={styles.pizzaIcon}>
+        <ImageOrSvg image={parallaxIconOne}/>
+        <ImageOrSvg image={parallaxIconTwo}/>
+      </div>
+    </div>
+  </section>;
 };
